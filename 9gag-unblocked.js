f1bfe9d703cc82d3cb35d12f619482f5b1ef4e75
Robin Gustafsson <robingustafsson93@gmail.com>
2014-08-12 16:10:36 +0200

    Added support for GIFs

--- 9gag-unblocked.js
+++ 9gag-unblocked.js
@@ -3,7 +3,7 @@
 * A bookmarklet to bypass the login requirement for viewing NSFW content on 9gag.
 *
 * Robin Gustafsson
-* 2014-08-10
+* 2014-08-12
 */
 
 (function(){
@@ -11,21 +11,30 @@
 	var nsfwElems = document.getElementsByClassName("badge-nsfw-entry-cover");
 
 	while (nsfwElems.length > 0) {
-		var elem = nsfwElems[0];
+		unblock(nsfwElems[0])
+	};
+
+	function unblock(elem) {
 		var parent = elem.parentNode;
 
 		var a = document.createElement("a");
 		if (window.location.href !== elem.href) {
 			a.href = elem.href;
 		}
+
+		var id = elem.href.substring(20); // "http://9gag.com/gag/".length == 20
 		
 		var img = document.createElement("img");
 		img.class = "badge-item-img";
-		img.src = "http://d24w6bsrhbeh9d.cloudfront.net/photo/" + elem.href.substring(20) + "_700b.jpg"; // "http://9gag.com/gag/".length == 20
+		img.src = "http://d24w6bsrhbeh9d.cloudfront.net/photo/" + id + "_460sa.gif";
+		img.onerror = function() {
+			this.onerror = null;
+			this.src = "http://d24w6bsrhbeh9d.cloudfront.net/photo/" + id + "_700b.jpg";
+		};
 		
 		parent.innerHTML = "";
 		a.appendChild(img);
 		parent.appendChild(a);
-	};
+	}
 
 })();
